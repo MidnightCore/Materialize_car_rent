d31522e2255c6baffb282a3ee0818cd13487537b
Alice <setsuna8741@gmail.com>
2019-09-22 13:22:41 +0700

    .

--- rentform.php
+++ rentform.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
-if(isset($_SESSION['id'])){
+if (isset($_SESSION['id'])) {
     $user_id = $_SESSION['id'];
-}else{
+} else {
     header("location:login.php");
-
 }
 require './server.php';
 
@@ -18,7 +17,6 @@ date_default_timezone_set("Asia/Bangkok");
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -67,6 +65,7 @@ date_default_timezone_set("Asia/Bangkok");
     </nav>
 
 
+
     <form action="rentform_form.php" id="nukKaew" method="POST">
         <!-- เริ่มต้นแบบฟอร์ม -->
         <div class="container">
@@ -78,13 +77,14 @@ date_default_timezone_set("Asia/Bangkok");
                         <h5 class="text-center">แบบฟอร์ม : ขออนุญาติใช้ยานพาหนะในเขตกรุงเทพฯและปริมณฑล</h5>
                     </h4>
                 </div>
-            </div><br><br><!-- จบหัวกระดาษ -->
+            </div><br><br>
+            <!-- จบหัวกระดาษ -->
 
 
 
             <div class="row">
+                <!-- ปุ่มกลมๆให้เลือก -->
                 <div class="input-field col s6">
-                    <!-- ปุ่มกลมๆให้เลือก -->
                     <p> <label>
                             <input name="county" type="radio" value="Bangkok" required />
                             <span>กรุงเทพฯและปริมณฑล </span>
@@ -94,35 +94,21 @@ date_default_timezone_set("Asia/Bangkok");
                             <span>ต่างจังหวัด </span>
                         </label>
                     </p>
-                </div><!-- จบปุ่มกลมๆให้เลือก -->
+                </div>
+                <!-- จบปุ่มกลมๆให้เลือก -->
 
+                <!-- เลือกวันที่กรอก -->
                 <div class="input-field col s6">
-                    <!-- เลือกวันที่กรอก -->
                     <input name="date_write" type="text" placeholder="วัน/เดือน/ปี (ที่กรอกแบบฟอร์ม)" value="<?php echo date("l j m Y H:i:s") ?>" readonly>
                 </div>
-            </div><!-- จบเลือกวันที่กรอก -->
-
-
-
-            <!-- scriptของเลือกวันที่ -->
-            <script>
-                document.addEventListener('DOMContentLoaded', function() {
-                    var elems = document.querySelectorAll('.datepicker');
-                    var instances = M.Datepicker.init(elems, options);
-                });
-                // Or with jQuery
-                $(document).ready(function() {
-                    $('.datepicker').datepicker();
-                });
-            </script><!-- จบscriptของเลือกวันที่ -->
+                <!-- จบเลือกวันที่กรอก -->
+            </div>
 
 
 
             <div class="row">
                 <div class="col s12">
                     <h6><b>เรียน</b> ผู้อำนวยการสำนักวิชาการศึกษาทั่วไปและนวัตกรรมการเรียนรู้อิเล็กทอรนิกส์</h6>
-
-
                     <!-- ส่วนของกรอกรายละเอียดชื่อ -->
                     <div class="row">
                         <div class="input-field col s6">
@@ -147,18 +133,20 @@ date_default_timezone_set("Asia/Bangkok");
                     <!-- จบส่วนของกรอกรายละเอียดชื่อ -->
 
 
+
+                    <!-- script dropdown -->
                     <script>
                         document.addEventListener('DOMContentLoaded', function() {
                             var elems = document.querySelectorAll('select');
                             var instances = M.FormSelect.init(elems, options);
                         });
-
                         // Or with jQuery
-
                         $(document).ready(function() {
                             $('select').formSelect();
                         });
                     </script>
+
+
 
                     <!-- ส่วนของสถานที่ -->
                     <p>มีความประสงค์จะขอใช้รถยนต์ของสำนักวิชาการศึกษาทั่วไปฯ เพื่อไปราชการเกี่ยวกับ</p>
@@ -167,7 +155,6 @@ date_default_timezone_set("Asia/Bangkok");
                         <div class="input-field col s6">
                             <p>สถานที่ไป<input type="text" name="place" class="P_80 mt-2" required></p>
                         </div>
-
                         <div class="input-field col s6"><br><br>
                             <select type="number" name="people" class="people_num" required>
                                 <option value="" disabled selected>จำนวนคนที่ไป</option>
@@ -204,6 +191,21 @@ date_default_timezone_set("Asia/Bangkok");
                             <input name="time_back" type="text" class="timepicker" placeholder="เวลา" required>
                         </div>
                     </div>
+
+
+
+                    <!-- scriptเลือกวันที่ -->
+                    <script>
+                        document.addEventListener('DOMContentLoaded', function() {
+                            var elems = document.querySelectorAll('.datepicker');
+                            var instances = M.Datepicker.init(elems, options);
+                        });
+                        // Or with jQuery
+                        $(document).ready(function() {
+                            $('.datepicker').datepicker();
+                        });
+                    </script>
+                    <!-- scriptเลือกเวลา -->
                     <script>
                         document.addEventListener('DOMContentLoaded', function() {
                             var elems = document.querySelectorAll('.timepicker');
@@ -214,7 +216,6 @@ date_default_timezone_set("Asia/Bangkok");
                             $('.timepicker').timepicker();
                         });
                     </script>
-                    <!-- จบวันที่ในการเดินทางไปกลับ -->
 
 
 
@@ -241,52 +242,8 @@ date_default_timezone_set("Asia/Bangkok");
                     <!-- จบลงชื่อคนขออณุญาติ -->
 
 
-
-                    <!-- ส่วนของเจ้าหน้าที่ -->
-                    <!--
-                    <h6><b>ส่วนนี้</b> เฉพาะเจ้าหน้าที่</h6>
-                    <p>
-                        <label>
-                            <input name="can_go" type="radio" value="agree" required />
-                            <span>สามารถให้บริการรถยนต์ได้</span>
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            <input name="can_go" type="radio" value="not_agree" required />
-                            <span>ไม่สามารถให้บริการรถยนต์ได้</span>
-                        </label>
-                    </p>
-                    <div>
-                        <p>เนื่องจาก <input type="text" class="reason"></p>
-                        <p>โดยมีพนักงานขับรถยนต์ ชื่อ <input name="driver_name" type="text" class="phone_num"></p>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <input name="driver_car" id="cartype" type="text" class="validate">
-                            <label for="cartype">ขับรถ (ประเภทรถ)</label>
-                        </div>
-                        <div class="input-field col s6">
-                            <input name="driver_carid" id="carnum" type="text" class="validate">
-                            <label for="carnum">หมายเลขทะเบียนรถ</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col s6 offset-s6">
-                            <div class="input-field inline">
-                                <input name="license_agent" id="name_agent" type="text" class="validate">
-                                <label for="name_agent">ลงชื่อ</label>
-                                <span class="helper-text" data-error="wrong"
-                                    data-success="right">เจ้าหน้าที่ฝ่ายยานพาหนะ</span>
-                            </div>
-                        </div>
-                    </div>
-                    -->
-                    <!-- จบส่วนของเจ้าหน้าที่ -->
-
                 </div>
             </div>
-
         </div>
     </form>
 
