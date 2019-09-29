7905f873bebd5ed9194439ac5e01a30bb41b00fc
Alice <setsuna8741@gmail.com>
2019-09-29 12:34:56 +0700

    .

--- admin_renovate/show_driver_car.php
+++ admin_renovate/show_driver_car.php
@@ -20,7 +20,9 @@ $result = mysqli_query($connect, $sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Show Driver</title>
+
+    <!-- CSS  -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
     <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
